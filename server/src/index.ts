f0ecc1686de0c62473108c1112a38d17b0ece5b9
Rony Freitas <ronyfreitas98@gmail.com>
2026-03-18 01:35:42 -0300

    chore: remove unused plugin boilerplate files

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,20 +10,17 @@ import register from "./register";
  */
 import config from "./config";
 import contentTypes from "./content-types";
-import controllers from "./controllers";
 import middlewares from "./middlewares";
 import policies from "./policies";
-import routes from "./routes";
-import services from "./services";
 
 export default {
   register,
   bootstrap,
   destroy,
   config,
-  controllers,
-  routes,
-  services,
+  controllers: {},
+  routes: {},
+  services: {},
   contentTypes,
   policies,
   middlewares,
